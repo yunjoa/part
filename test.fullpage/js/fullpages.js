60c87efd3f10ad7248eb05c24b1856ebf3188fc9
JH <52683681+yunjoa@users.noreply.github.com>
2024-06-08 18:01:10 +0900

    고쳤다
    오픈라이선스 + 경로

---
+++ test.fullpage/js/fullpages.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  new fullpage("#fullpage", {
+    // licenseKey: 'gplv3-license', // 오픈 소스 버전 라이선스 ㄴㄴ
+    anchors: ["page1", "page2", "page3", "page4"],
+    sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+  });
+});
