d270422744a3aed6f1b938ecdc7b6a3d7ea96211
JH <52683681+yunjoa@users.noreply.github.com>
2024-06-08 17:45:28 +0900

    fullpage.test

---
+++ test.fullpage/js/fullpage.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  new fullpage("#fullpage", {
+    licenseKey: "gplv3-license",
+    anchors: ["page1", "page2", "page3", "page4"],
+    sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+  });
+});
