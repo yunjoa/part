1d19cdad91703a9328759ae2f59760ece51f0dc8
JH <52683681+yunjoa@users.noreply.github.com>
2024-07-13 20:14:27 +0900

    Update script.js
    타이틀이 있을 때는 style 내용 끝에 따옴표가 하나 찍히는데 title로 변환할  alt 값이 없으면 style 끝에 따옴표가 두개 찍히는 거 수정.. 아 이제 수정 그만 ㅠㅠㅠㅠ

--- incruit_imgmap/script.js
+++ incruit_imgmap/script.js
@@ -9,37 +9,36 @@ function convertToALink() {
     }
 
     
-            var regex = /<area shape="(rect|circle|poly)" coords="([^"]+)" href="([^"]+)"(?: target="([^"]+)")?(?: alt="([^"]+)")?>/g;
-            var convertedText = '<div style="position:relative;">';
+        var regex = /<area shape="(rect|circle|poly)" coords="([^"]+)" href="([^"]+)"(?: target="([^"]+)")?(?: alt="([^"]+)")?>/g;
+    var convertedText = '<div style="position:relative;">';
 
+    convertedText += inputText.replace(regex, function(match, shape, coords, href, target, alt) {
+        var coordsArray = coords.split(',').map(Number);
+        var aTag = '';
+
+        if (shape === 'rect') {
+            var leftPercent = (coordsArray[0] / imageWidth) * 100;
+            var topPercent = (coordsArray[1] / imageHeight) * 100;
+            var rightPercent = (coordsArray[2] / imageWidth) * 100;
+            var bottomPercent = (coordsArray[3] / imageHeight) * 100;
+            var widthPercent = Math.round((rightPercent - leftPercent) * 100) / 100;
+            var heightPercent = Math.round((bottomPercent - topPercent) * 100) / 100;
+
+            aTag = '<a href="' + href + '" style="position: absolute; left: ' + leftPercent.toFixed(2) + '%; top: ' + topPercent.toFixed(2) + '%; width: ' + widthPercent + '%; height: ' + heightPercent + '%; background: red;"';
             
-
-            convertedText += inputText.replace(regex, function(match, shape, coords, href, target, alt) {
-                var coordsArray = coords.split(',').map(Number);
-                var aTag = '';
-
-                if (shape === 'rect') {
-                    var leftPercent = (coordsArray[0] / imageWidth) * 100;
-                    var topPercent = (coordsArray[1] / imageHeight) * 100;
-                    var rightPercent = (coordsArray[2] / imageWidth) * 100;
-                    var bottomPercent = (coordsArray[3] / imageHeight) * 100;
-                    var widthPercent = Math.round((rightPercent - leftPercent) * 100) / 100;
-                    var heightPercent = Math.round((bottomPercent - topPercent) * 100) / 100;
-
-                    aTag = '<a href="' + href + '" style="position: absolute; left: ' + leftPercent.toFixed(2) + '%; top: ' + topPercent.toFixed(2) + '%; width: ' + widthPercent + '%; height: ' + heightPercent + '%; background: red;" ';
             if (alt) {
-                aTag += ' title="' + alt.replace(/"/g, '&quot;');  
+                aTag += ' title="' + alt.replace(/"/g, '&quot;') + '"';
             }
-            aTag += '"></a>';
-               
-                }
-
-                return aTag;
-            });
-
-            convertedText += '</div>';
-            document.getElementById('outputTextarea').value = convertedText;
+            
+            aTag += '></a>';
         }
+
+        return aTag;
+    });
+
+    convertedText += '</div>';
+    document.getElementById('outputTextarea').value = convertedText;
+}
 
         function copyToClipboard() {
             var outputTextarea = document.getElementById('outputTextarea');
